ef569f14aca0a1bb9a3fd9a821c0ca36f81232e8
indahFadhila <indahfadhila84@gmail.com>
2026-09-22 12:59:24 +0700

    Icon purpose 'any' aja (nggak maskable) + bump SW v8

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategy: cache-first buat asset app-shell, network-first buat rest.
 // Kalau update file, bump CACHE_VERSION supaya user dapet versi baru.
 
-const CACHE_VERSION = "jadwal-v7";
+const CACHE_VERSION = "jadwal-v8";
 const APP_SHELL = [
   "./",
   "./index.html",
